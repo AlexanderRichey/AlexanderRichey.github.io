9befe76eaa1c8959a00231c9379ae7e551d051f7
Alexander Richey <alexander.richey@gmail.com>
2017-12-03 17:18:03 -0500

    fix bug where bad links were being created

--- lib/builder.py
+++ lib/builder.py
@@ -1,4 +1,5 @@
-from os import listdir
+import os
+import sys
 from os.path import dirname, abspath, join, splitext
 from pathlib import Path
 from io import BytesIO
@@ -27,24 +28,29 @@ except Exception as e:
     raise ImportError('Could not load template directory')
 
 md = Markdown(extensions=['markdown.extensions.meta'])
+article_template = jinja_env.get_template('article.html')
+index_template = jinja_env.get_template('index.html')
 
 
 def build():
-    Path(join(DIST_DIR, 'articles')).mkdir(exist_ok=True)
-    article_template = jinja_env.get_template('article.html')
-    index_template = jinja_env.get_template('index.html')
-    fnames = sorted(listdir(path=ARTICLES_DIR),
-                    key=lambda f: datetime(*[int(i) for i in f.split('-')[:3]]).timestamp(),
-                    reverse=True)
+    sys.stdout.write('Building... ')
+    clean_dist_dir()
+    mkdir_in_dist('articles')
+
+    fnames = sorted(
+        os.listdir(path=ARTICLES_DIR),
+        key=lambda f: datetime(*[int(i) for i in f.split('-')[:3]]).timestamp(),
+        reverse=True
+    )
+
     index_page_num = 0
     index_payload = []
     len_articles = len(fnames)
     max_page_len = CONFIG.get('articles_per_page', 3)
+
     for idx, in_fname in enumerate(fnames):
         base_fname, _ = splitext(in_fname)
-        out_fname = base_fname + '.html'
         in_fpath = join(ARTICLES_DIR, in_fname)
-        out_fpath = join(DIST_DIR, 'articles', out_fname)
 
         buf = BytesIO()
         md.convertFile(input=in_fpath, output=buf)
@@ -53,6 +59,8 @@ def build():
 
         article_title, page_title, date, url = \
             massage_metadata(meta, base_fname)
+
+        out_fpath = join(DIST_DIR, url[1:])
 
         article_payload = dict(
             url=url,
@@ -67,26 +75,27 @@ def build():
 
         index_payload.append(article_payload)
 
-        if (((idx + 1) % max_page_len) == 0 and idx != 0) or (idx == (len_articles - 1)):
+        if (((idx + 1) % max_page_len) == 0 and idx != 0) or \
+                (idx == (len_articles - 1)):
             page_title = CONFIG.get('name', '')
             if index_page_num == 0:
                 out_index_fpath = join(DIST_DIR, 'index.html')
                 url = BASE_URL
                 prev_page = None
             else:
-                page_dir = 'page' + str(index_page_num)
+                page_dir = 'page' + str(index_page_num + 1)
+                mkdir_in_dist(page_dir)
                 url = '/' + page_dir
                 out_index_fpath = join(DIST_DIR, page_dir, 'index.html')
-                Path(join(DIST_DIR, page_dir)).mkdir(exist_ok=True)
             if (len_articles - 1) > idx:
-                next_page = '/page' + str(index_page_num + 1)
+                next_page = '/page' + str(index_page_num + 2)
             elif (len_articles - 1) == idx:
                 next_page = None
             if index_page_num > 0:
                 if index_page_num == 1:
                     prev_page = '/'
                 else:
-                    prev_page = '/page' + str(index_page_num - 1)
+                    prev_page = '/page' + str(index_page_num)
             with open(out_index_fpath, 'w') as out_fp:
                 out_fp.write(index_template.render(
                     url=url,
@@ -97,14 +106,27 @@ def build():
                 ))
             index_payload = []
             index_page_num += 1
+    sys.stdout.write('Done\n')
 
 
 def massage_metadata(meta, base_fname):
     article_title = ','.join(meta.get('title', ''))
-    url = '/articles/' + slugify(article_title) + '.html'
+    url = join('/', 'articles', slugify(article_title) + '.html')
     page_title = CONFIG.get('name', '') + ' • ' + article_title
     date = '-'.join(base_fname.split('-')[:3])
     return article_title, page_title, date, url
+
+
+def mkdir_in_dist(name):
+    Path(join(DIST_DIR, name)).mkdir(exist_ok=True)
+
+
+def clean_dist_dir():
+    for root, dirs, files in os.walk(DIST_DIR, topdown=False):
+        for name in files:
+            os.remove(join(root, name))
+        for name in dirs:
+            os.rmdir(join(root, name))
 
 
 if __name__ == '__main__':

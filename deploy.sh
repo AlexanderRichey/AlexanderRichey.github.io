cb8ac7a3cba0e6f973c68f02ee441f1a38413225
AlexanderRichey <alexander.richey@gmail.com>
2019-11-04 06:23:27 +0000

    add docker commands article, update deploy.sh for new server

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,6 @@
+set -ex
 source env/bin/activate
 python3 builder.py build
-mkdir -p tmp
-cd tmp
-git clone git@github.com:AlexanderRichey/AlexanderRichey.github.io.git
-cd AlexanderRichey.github.io
-git co master
-rm -rf *
-cp -r ../../dist/* .
-git add .
-git ci -m "Update site"
-git push
-cd ../../
-rm -rf tmp
+sudo rm -rf /var/www/alexrichey.com/*
+sudo cp -r dist/* /var/www/alexrichey.com/
+sudo systemctl restart nginx
